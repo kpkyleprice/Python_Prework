d0be11321ee26fabb4ebd8f62d55233ca5bc9fa0
Kyle Price <kpkyleprice@gmail.com>
2022-12-04 18:35:18 -0800

    final commit

---
+++ Q1.py
@@ -0,0 +1,4 @@
+def hello_name(USER_NAME):
+    print("hello_"+USER_NAME + "!")
+user_name = input('Enter USERNAME: ')
+hello_name(user_name)
---
+++ Q2.py
@@ -0,0 +1,5 @@
+def first_odd():
+    for i in range(1,101):
+        if i % 2 != 0:
+            print(i)
+first_odd()
